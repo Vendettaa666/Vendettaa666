f75b1485b982f0d8450a402731aa5dcdbe5ed3d2
vendettaa666 <vinnfamily666@gmail.com>
2025-09-03 22:16:41 +0700

    add: icon library and hero page

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,29 @@
+import DataImage from "./data";
+
 function App() {
   return (
     <>
-      <h1>
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original f
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackhamorm, accompanied by English versions from the 1914 translation by H. Rackham.
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sectionsv
-The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackhamaccordion-accordion</h1>
+      <div className="hero grid md:grid-cols-2 pt-10 items-center xl:gap-0 gap-6 grid-cols-1">
+        <div>
+          <div className="display flex items-center gap-3 mb-6 bg-zinc-800 w-fit p-4 rounded-2xl">
+            <img
+              src={DataImage.HeroImage}
+              alt="Hero Image"
+              className="w-10 rounded-md"/>
+            <q>Usaha tidak menghianati Hasil</q>
+          </div>
+          <h1 className="text-5xl/tight font-bold mb-6">Hi,Saya Leo satria</h1>
+          <p className="text-base/loose mb-6 opacity-50">Berdasarkan jalur yang Anda berikan, berikut adalah perintah lengkap yang bisa Anda jalankan di terminal Laragon untuk menimpa database smesta_id dengan file SQL yang baru. Setelah Anda menjalankan perintah ini, tunggu beberapa saat. Tergantung pada ukuran file .sql, proses ini mungkin memakan waktu beberapa detik hingga beberapa menit. Jika tidak ada pesan error yang muncul, berarti proses penimpaan database telah berhasil.
+          </p>
+          <div className="flex items-center sm:gap-4 gap-2">
+            <a href="" className="bg-violet-700 p-4 rounded-2xl hover:bg-violet-600">Download CV <i className="ri-download-line ri-lg"></i></a>
+            <a href="" className="bg-zinc-700 p-4 rounded-2xl hover:bg-zinc-600">Lihat Proyek <i className="ri-arrow-down-line ri-lg"></i></a>
+          </div>
+        </div>
+        <img src={DataImage.HeroImage} alt="Hero Image" className="w-[400px] md:ml-auto" />
+      </div>
     </>
-  )
+  );
 }
 
 export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,50 +1,50 @@
 import { useState, useEffect } from "react";
 const Navbar = () => {
-    const [active, setActive] = useState(false);
-    useEffect(() => {
-        const handleScroll = () => {
-            if(window.scrollY > 150){
-                setActive(true);}
-            else{
-                setActive(false);
-            }
-        };
-        window.addEventListener('scroll', handleScroll);
-        return () => {
-            window.removeEventListener('scroll', handleScroll);
-        };
-    }, []);
-    return (
-      <div className="navbar py-7 flex justify-between items-center">
-        <div className="logo">
-          <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white">Portofolio</h1>
-        </div>
-        <ul className={`menu flex flex-row items-center gap-4 sm:gap-6 md:static fixed left-1/2 -translate-x-1/2 md:-translate-x-0 md:opacity-100 bg-white/30 backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl md:bg-transparent transition-all duration-300 whitespace-nowrap ${
-    active ? "top-0 opacity-100 z-50" : "-top-20 opacity-0"
-  }`}>
-          <li>
-            <a href="" className="sm:text-lg text-base font-medium">
-              Home
-            </a>
-          </li>
-          <li>
-            <a href="" className="sm:text-lg text-base font-medium">
-              About me
-            </a>
-          </li>
-          <li>
-            <a href="" className="sm:text-lg text-base font-medium">
-              Skills
-            </a>
-          </li>
-          <li>
-            <a href="" className="sm:text-lg text-base font-medium">
-              Contact me
-            </a>
-          </li>
-        </ul>
+  const [active, setActive] = useState(false);
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 150) {
+        setActive(true);
+      }
+      else {
+        setActive(false);
+      }
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+  return (
+    <div className="navbar py-7 flex justify-between items-center">
+      <div className="logo">
+        <h1 className="text-3xl font-bold bg-white text-black p-1 md:bg-transparent md:text-white">Portofolio</h1>
       </div>
-    );
+      <ul className={`menu flex flex-row items-center gap-4 sm:gap-6 md:static fixed left-1/2 -translate-x-1/2 md:-translate-x-0 md:opacity-100 bg-white/30 backdrop-blur-md p-4 rounded-br-2xl rounded-bl-2xl md:bg-transparent transition-all duration-300 whitespace-nowrap ${active ? "top-0 opacity-100 z-50" : "-top-20 opacity-0"
+        }`}>
+        <li>
+          <a href="" className="sm:text-lg text-base font-medium">
+            Home
+          </a>
+        </li>
+        <li>
+          <a href="" className="sm:text-lg text-base font-medium">
+            About me
+          </a>
+        </li>
+        <li>
+          <a href="" className="sm:text-lg text-base font-medium">
+            Skills
+          </a>
+        </li>
+        <li>
+          <a href="" className="sm:text-lg text-base font-medium">
+            Contact me
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
 }
 
 export default Navbar
